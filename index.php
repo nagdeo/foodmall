f38f247c7f64f82e31b69f6c0a1158dd8a20865e
nagdeo <simminagdeo@gmail.com>
2020-06-22 00:01:13 +0530

    foodmall

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $conn = Connect();
   <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type = "text/css" href ="css/bootstrap-4.5.0-dist/css/bootstrap.min.css">
   <script type="text/javascript" src="js/jquery.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src=" js/bootstrap.min.js"></script>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
